e70dfe5b9646c14f16cec31c1028e770f0ee378b
Lucas-allard <allard.lucas.97@gmail.com>
2023-03-02 17:29:35 +0100

    update signatures

---
+++ public/signature.php
@@ -0,0 +1,214 @@
+<table width="523px" border="0" cellspacing="0" cellpadding="0">
+    <tbody>
+    <tr>
+        <td width="155" rowspan="2" valign="middle" class="w1" style="padding: 0 0 0 20px">
+            <img class="w1"
+                 src="https://mili-atlas.fr/wp-content/uploads/Jean-Pierre-COHIGNAC-signature-e1677768584961.png"
+                 alt="Jean-Pierre COHIGNAC"
+                 width="120px"
+                 style="display: block; border-radius: 50%;">
+        </td>
+        <td width="258" rowspan="2" colspan="2" style="padding-left: 10px;">
+            <p style="font-family: 'Montserrat Medium', sans-serif; font-size: 11px; line-height:15px; font-weight: bold; margin:0; padding:0; color: inherit;">
+                <span style="margin-right: 2em">Jean-Pierre COHIGNAC</span>
+                <a href="https://www.linkedin.com/in/jeanpierrecohignac/">
+                    <img style="width:20px; height:20px"
+                         src="https://mili-atlas.fr/wp-content/uploads/Icones_Reseaux_in.png"
+                         alt="Profil linkedIn"
+                    >
+                </a>
+            </p>
+            <p style="font-family: 'Montserrat Regular', sans-serif; font-size: 11px; line-height:15px; margin:0; padding:0; color: inherit; ">
+                <span style="margin-right: 2em ; color: inherit; ">Négociateur Immobilier</span>
+            </p>
+
+            <hr width="150" align="left" margin-right="40px">
+
+            <p style="font-family: 'Montserrat Regular', sans-serif; font-size: 11px; line-height:15px; margin:0 0 2px 0; padding:0;  color: inherit; ">
+                <a href="tel:0680012119"
+                   style="text-decoration:none;  color: inherit; "
+                >06 80 01 21 19</a></p>
+            <p style="font-family: 'Montserrat Regular', sans-serif; font-size: 11px; line-height:11px; margin:0 0 2px 0; padding:0;  color: inherit; ">
+                <a href="mailto:jp.cohignac@immodef.fr"
+                   style="text-decoration:none;  color: inherit; ">jp.cohignac@immodef.fr</a>
+            </p>
+            <p style="font-family: 'Montserrat Medium', sans-serif; font-size: 11px; line-height:11px; font-weight: bold; margin:0; padding:0; color: inherit; ">
+                <a href="www.immodef.fr"
+                   style="text-decoration: none; color: inherit; ; font-weight: 500">www.immodef.fr</a>
+            </p>
+        </td>
+    </tr>
+    <tr>
+        <td class="w2" height="30" width="125" valign="top">
+            <a href="www.immodef.fr">
+                <img class="light-img"
+                     src="https://mili-atlas.fr/wp-content/uploads/Atlas-immodef.png"
+                     alt="Mili Atlas" style="display: block;" width="125px">
+            </a>
+        </td>
+    </tr>
+    <tr>
+        <td></td>
+        <td colspan="4">
+            <p style="font-family: 'Montserrat Medium', sans-serif; font-size: 11px; line-height:7px; word-spacing: 2px; margin:0; padding: 10px 0 10px 10px; color: inherit; ">
+                <small style="font-size: 0.4rem">
+                    SAS IMMODEF au capital de 1000€ <br> Carte pro CPI 4401 2021 000 000 012
+                    délivrée par la CCI de Nantes-Saint Nazaire - SIREN 892 146 911
+                </small>
+            </p>
+        </td>
+
+    </tr>
+    </tbody>
+</table>
+
+<pre class="border border-4 w-75  m-auto" style="height: 200px">
+                            <code class="text-danger">
+&lt;style&gt;
+    hr {
+        margin: 1rem 0 !important;
+    }
+
+    .mobile {
+        display: none !important;
+    }
+
+    .mobileplein {
+        display: none !important;
+    }
+
+    a[x-apple-data-detectors] {
+        color: inherit !important;
+        text-decoration: none !important;
+        font-size: inherit !important;
+        font-family: inherit !important;
+        font-weight: inherit !important;
+        line-height: inherit !important;
+    }
+
+    @media only screen and (max-width: 600px) {
+        .bck {
+            background-position: 0 134px !important;
+        }
+
+        .tm {
+            width: 310px !important;
+        }
+
+        img.tm {
+            height: auto !important;
+        }
+
+        .tm2 {
+            width: 260px !important;
+        }
+
+        img.tm2 {
+            height: auto !important;
+        }
+
+        .desktop {
+            display: none !important;
+        }
+
+        .block {
+            display: block !important;
+        }
+
+        .mobile {
+            display: block !important;
+        }
+
+        .centre {
+            margin-left: auto !important;
+            margin-right: auto !important;
+        }
+
+        .tcentre {
+            text-align: center;
+        }
+
+        .w1 {
+            width: 80px !important;
+            height: auto !important
+        }
+
+        .w2 {
+            width: 150px !important
+        }
+
+        .w3 {
+            width: 80px !important;
+            height: auto !important
+        }
+
+        .w4 {
+            width: 10px !important
+        }
+
+        .ml {
+            width: 215px !important;
+            height: auto !important;
+        }
+    }
+&lt;/style&gt;
+&lt;table width="523px" border="0" cellspacing="0" cellpadding="0"&gt;
+    &lt;tbody&gt;
+    &lt;tr&gt;
+        &lt;td width="155" rowspan="2" valign="middle" class="w1" style="padding: 0 0 0 20px"&gt;
+            &lt;img class="w1"
+                 src="https://mili-atlas.fr/wp-content/uploads/Jean-Pierre-COHIGNAC-signature-e1677768584961.png"
+                 alt="Jean-Pierre COHIGNAC"
+                 width="120px"
+                 style="display: block; border-radius: 50%;"&gt;
+        &lt;/td&gt;
+        &lt;td width="258" rowspan="2" colspan="2" style="padding-left: 10px;"&gt;
+            &lt;p style="font-family: 'Montserrat Medium', sans-serif; font-size: 11px; line-height:15px; font-weight: bold; margin:0; padding:0; color: inherit;"&gt;
+                Jean-Pierre COHIGNAC
+            &lt;/p&gt;
+            &lt;p style="font-family: 'Montserrat Regular', sans-serif; font-size: 11px; line-height:15px; margin:0; padding:0; color: inherit; "&gt;
+                &lt;span style="margin-right: 2em ; color: inherit; "&gt;Négociateur Immobilier&lt;/span&gt;
+            &lt;/p&gt;
+
+            &lt;hr width="150" align="left" margin-right="40px"&gt;
+
+            &lt;p style="font-family: 'Montserrat Regular', sans-serif; font-size: 11px; line-height:15px; margin:0 0 2px 0; padding:0;  color: inherit; "&gt;
+                &lt;a href="tel:0680012119"
+                   style="text-decoration:none;  color: inherit; "
+                &gt;06 80 01 21 19&lt;/a&gt;&lt;/p&gt;
+            &lt;p style="font-family: 'Montserrat Regular', sans-serif; font-size: 11px; line-height:11px; margin:0 0 2px 0; padding:0;  color: inherit; "&gt;
+                &lt;a href="mailto:jp.cohignac@immodef.fr"
+                   style="text-decoration:none;  color: inherit; "&gt;jp.cohignac@immodef.fr&lt;/a&gt;
+            &lt;/p&gt;
+            &lt;p style="font-family: 'Montserrat Medium', sans-serif; font-size: 11px; line-height:11px; font-weight: bold; margin:0; padding:0; color: inherit; "&gt;
+                &lt;a href="https://www.mili-atlas.fr"
+                   style="text-decoration: none; color: inherit; ; font-weight: 500"&gt;www.immodef.fr&lt;/a&gt;
+            &lt;/p&gt;
+        &lt;/td&gt;
+    &lt;/tr&gt;
+    &lt;tr&gt;
+        &lt;td class="w2" height="30" width="100" valign="bottom"&gt;
+            &lt;a href="https://www.mili-atlas.fr"&gt;
+                &lt;img class="light-img"
+                     src="https://mili-atlas.fr/wp-content/uploads/Immodef.png"
+                     alt="Mili Atlas" style="display: block;" width="100px"&gt;
+            &lt;/a&gt;
+        &lt;/td&gt;
+    &lt;/tr&gt;
+    &lt;tr&gt;
+        &lt;td&gt;&lt;/td&gt;
+        &lt;td colspan="4"&gt;
+            &lt;p style="font-family: 'Montserrat Medium', sans-serif; font-size: 11px; line-height:6px; word-spacing: 2px; margin:0; padding: 10px 0 10px 10px; color: inherit; "&gt;
+                &lt;small style="font-size: 0.4rem"&gt;
+                    SAS IMMODEF au capital de 1000€ - Carte pro CPI 4401 2021 000 000 012
+                    délivrée par la CCI de Nantes-Saint Nazaire - SIREN 892 146 911
+                &lt;/small&gt;
+            &lt;/p&gt;
+        &lt;/td&gt;
+
+    &lt;/tr&gt;
+    &lt;/tbody&gt;
+&lt;/table&gt;
+
+                            </code>
+                        </pre>
